752d6ab47cc44fd6430fb977a67128b7407e3a0e
Evgenii Mikhailov <e.mikhailov@outlook.com>
2023-04-14 14:47:07 +0300

    Initial Commit

---
+++ CryptoApp/CryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAppApp.swift
+//  CryptoApp
+//
+//  Created by Evgenii Mikhailov on 14.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
