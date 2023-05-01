5a6be7091d7a6aba036a402832d9f146c3ced78a
Evgenii Mikhailov <e.mikhailov@outlook.com>
2023-05-01 14:56:08 +0300

    Caching images implemented

--- CryptoApp/Services/CoinImageService.swift
+++ CryptoApp/Services/CoinImageService.swift
@@ -14,26 +14,42 @@ class CoinImageService {
     
     private var imageSubscription: AnyCancellable?
     private let coin: CoinModel
+    private let fileManager = LocalFileManager.instance
+    private let folderName = "coin_images"
+    private let imageName: String
     
     
     @Published var image: UIImage? = nil
     
     init(coin: CoinModel) {
         self.coin = coin
+        self.imageName = coin.id
         getCoinImage()
     }
     
     private func getCoinImage() {
-        guard let url = URL(string: coin.image)
-        else { return }
+        if let savedImage = fileManager.loadImage(imageName: imageName, folderName: folderName) {
+            image = savedImage
+            print("Retrieved iamge from FM")
+        } else {
+            downloadCoinImage()
+            print("Loading image")
+        }
+    }
+    
+    private func downloadCoinImage() {
+      
+        guard let url = URL(string: coin.image) else { return }
         
         imageSubscription =  NetworkingManager.download(url: url)
             .tryMap({ (data) -> UIImage? in
                 return UIImage(data: data)
             })
             .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self] image in
-                self?.image = image
-                self?.imageSubscription?.cancel()
+                guard let self = self, let image = image else { return }
+                self.image = image
+                self.imageSubscription?.cancel()
+                self.fileManager.saveImage(image: image, imageName: self.imageName, folderName: self.folderName)
             })
     }
 }
